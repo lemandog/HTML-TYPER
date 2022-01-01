f5002324f357c96deb57562a3ccaa797e20590fb
Lemandog <alexo98288@gmail.com>
2022-01-02 00:35:01 +0400

    code cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("/mainWindow.fxml"));
---
+++ src/main/java/WrapMain.java
@@ -0,0 +1,5 @@
+public class WrapMain {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
